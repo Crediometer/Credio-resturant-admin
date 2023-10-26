7715fb4baefcd01928f1dc3b112abb45cae8da31
Precious1324 <aleriwaprecious70@gmail.com>
2023-10-26 10:55:27 +0100

    latest upadate

--- src/Pages/Settings/Account.js
+++ src/Pages/Settings/Account.js
@@ -1,10 +1,11 @@
 import { HiOutlineUser } from 'react-icons/hi';
 import {HiOutlineEnvelope} from 'react-icons/hi2'
 import ng from '../../Assets/ng.png'
-import profile from '../../Assets/profile.png'
+import profile from '../../Assets/profile2.png'
 import './Account.css'
 import { IoLocationOutline } from 'react-icons/io5';
 import { FiChevronDown } from 'react-icons/fi';
+import {FaRegUser} from 'react-icons/fa'
 const Account = () => {
     return ( 
         <div className="account">
@@ -49,22 +50,31 @@ const Account = () => {
                             <div className="director-form">
                                 <label className='form-label'>First Name</label><br></br>
                                 <div className="director-form-input">
-                                    <HiOutlineUser/>
-                                    <input type='text' placeholder='First Name'></input>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="21" viewBox="0 0 17 21" fill="none">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.48298 13.8457C4.61536 13.8457 1.3125 14.4305 1.3125 16.7724C1.3125 19.1143 4.5944 19.72 8.48298 19.72C12.3506 19.72 15.6525 19.1343 15.6525 16.7933C15.6525 14.4524 12.3715 13.8457 8.48298 13.8457Z" stroke="#5E6366" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.48684 10.5059C11.0249 10.5059 13.0821 8.44779 13.0821 5.90969C13.0821 3.3716 11.0249 1.31445 8.48684 1.31445C5.94875 1.31445 3.89065 3.3716 3.89065 5.90969C3.88208 8.43922 5.92589 10.4973 8.45446 10.5059H8.48684Z" stroke="#5E6366" stroke-width="1.42857" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                    <input type='text' placeholder='Test'></input>
                                 </div>
                             </div>
                             <div className="director-form">
                                 <label>Last Name</label><br></br>
                                 <div className="director-form-input">
-                                    <HiOutlineUser/>
-                                    <input type='text' placeholder='Last Name'></input>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="21" viewBox="0 0 17 21" fill="none">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.48298 13.8457C4.61536 13.8457 1.3125 14.4305 1.3125 16.7724C1.3125 19.1143 4.5944 19.72 8.48298 19.72C12.3506 19.72 15.6525 19.1343 15.6525 16.7933C15.6525 14.4524 12.3715 13.8457 8.48298 13.8457Z" stroke="#5E6366" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.48684 10.5059C11.0249 10.5059 13.0821 8.44779 13.0821 5.90969C13.0821 3.3716 11.0249 1.31445 8.48684 1.31445C5.94875 1.31445 3.89065 3.3716 3.89065 5.90969C3.88208 8.43922 5.92589 10.4973 8.45446 10.5059H8.48684Z" stroke="#5E6366" stroke-width="1.42857" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                    <input type='text' placeholder='Venture'></input>
                                 </div>
                             </div>
                             <div className="director-form">
                                 <label>Email</label><br></br>
                                 <div className="director-form-input">
-                                    <HiOutlineEnvelope/>
-                                    <input type='text' placeholder='Email'></input>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="21" viewBox="0 0 23 21" fill="none">
+                                    <path d="M17.4014 7.35156L12.9581 10.9646C12.1186 11.6306 10.9375 11.6306 10.098 10.9646L5.61719 7.35156" stroke="#5E6366" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.4089 19.5C19.4502 19.5084 21.5 17.0095 21.5 13.9384V7.07001C21.5 3.99883 19.4502 1.5 16.4089 1.5H6.59114C3.54979 1.5 1.5 3.99883 1.5 7.07001V13.9384C1.5 17.0095 3.54979 19.5084 6.59114 19.5H16.4089Z" stroke="#5E6366" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                    <input type='text' placeholder='TestVenture@gmail.com'></input>
                                 </div>
                             </div>
                             <div className="director-form ">
